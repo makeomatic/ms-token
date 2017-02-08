fe0ca87a5a4195194216c5728ba446ddc4de341e
AVVS <v@makeomatic.ru>
2017-02-07 21:00:37 -0800

    chore: helper to decrypt tokens for verification reasons

---
+++ bin/decrypt.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-console */
+
+// decodes input token
+const assert = require('assert');
+const crypto = require('../lib/utils/crypto');
+
+//
+// Logic
+//
+const [,, token, algorithm, sharedSecret] = process.argv;
+
+assert.ok(token, 'pass token as first arg');
+assert.ok(algorithm, 'pass algo as second arg');
+assert.ok(sharedSecret, 'pass shared secret as third arg');
+
+const { decrypt } = crypto.createCipher({ algorithm, sharedSecret });
+
+// decrypted
+const decrypted = decrypt(token);
+
+console.info('\ndecrypted: \n', decrypted, '\n');
+
+const parsed = JSON.parse(decrypted);
+
+console.info('\nparsed JSON: \n', parsed, '\n');
+
+process.exit(0);
